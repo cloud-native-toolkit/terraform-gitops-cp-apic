b22fcd1e34f8b34e6e044cb744995b12197c7831
vijayks <vsukthankar@yahoo.com>
2022-03-10 14:18:48 +0530

    Initial checkin
    Signed-off-by: vijayks <vsukthankar@yahoo.com>

--- test/stages/stage2-apic-instance.tf
+++ test/stages/stage2-apic-instance.tf
@@ -1,9 +1,13 @@
 module "gitops_module" {
   source = "./module"
-
+  depends_on = [module.apic,module.cp4i-dependencies]
   gitops_config = module.gitops.gitops_config
   git_credentials = module.gitops.git_credentials
   server_name = module.gitops.server_name
   namespace = module.gitops_namespace.name
   kubeseal_cert = module.gitops.sealed_secrets_cert
+  entitlement_key = var.cp_entitlement_key
+  apic_version = module.cp4i-dependencies.apic.version
+  license_id = module.cp4i-dependencies.apic.license
+  usage = module.cp4i-dependencies.apic.license_use
 }
